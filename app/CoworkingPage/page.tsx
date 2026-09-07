660b011905d196a9cfe84930e4b0a55e0f2d93db
Chelsea <s094334@gmail.com>
2026-09-07 18:03:40 +0800

    fix: move CoworkingPage and RocketPage to app folder

---
+++ app/CoworkingPage/page.tsx
@@ -0,0 +1,15 @@
+import SpaceIntro from "../../src/components/SpaceIntro";
+import SpaceDescription from "../../src/components/SpaceDescription";
+import SpaceCarousel from "../../src/components/SpaceCarousel";
+import SpaceFee from "../../src/components/SpaceFee";
+
+export default function CoworkingPage() {
+  return (
+    <>
+      <SpaceIntro />
+      <SpaceDescription />
+      <SpaceCarousel />
+      <SpaceFee />
+    </>
+  );
+}
