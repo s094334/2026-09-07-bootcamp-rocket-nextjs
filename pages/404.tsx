8d5a7f1c633e70482d4416bc31191a39ddf12999
Chelsea <s094334@gmail.com>
2026-09-09 15:27:24 +0800

    feat: add Head for home, coworking and 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,16 +1,23 @@
 import Link from "next/link";
+import Head from "next/head";
 
 export default function Custom404() {
   return (
-    <div>
-      <h2 className="font-bold text-mobile-h2 md:text-desktop-h2 text-center pt-10">
-        404 找不到頁面
-      </h2>
-      <Link href="/" className="text-center">
-        <h3 className="font-bold text-mobile-h3 py-5 md:text-desktop-h3 text-neutral-700 md:py-6 hover:text-neutral-500">
-          回到火箭隊首頁
-        </h3>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <meta name="description" content="Practicing Next.js — 404 Not found" />
+        <title>404 找不到頁面</title>
+      </Head>
+      <div>
+        <h2 className="font-bold text-mobile-h2 md:text-desktop-h2 text-center pt-10">
+          404 找不到頁面
+        </h2>
+        <Link href="/" className="text-center">
+          <h3 className="font-bold text-mobile-h3 py-5 md:text-desktop-h3 text-neutral-700 md:py-6 hover:text-neutral-500">
+            回到火箭隊首頁
+          </h3>
+        </Link>
+      </div>
+    </>
   );
 }
